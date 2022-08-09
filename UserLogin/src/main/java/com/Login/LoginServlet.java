7af8240611e87a73fc04e21dd3f1aaabb0a1bb89
Rajeshwaranj <110368828+Rajeshwaranj@users.noreply.github.com>
2022-08-09 22:36:11 +0530

    Add files via upload

---
+++ UserLogin/src/main/java/com/Login/LoginServlet.java
@@ -0,0 +1,48 @@
+package com.Login;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class DemoServlet
+ */
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter pw=response.getWriter();
+		String user=request.getParameter("user");
+		String pwd=request.getParameter("pwd");
+		if(user.equals(pwd)) {
+			pw.print("login successfull");
+		}
+		else {
+			pw.print("login failed");
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
