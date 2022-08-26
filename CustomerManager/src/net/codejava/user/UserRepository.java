a45aebca233315687935979e31be927c4c4599d1
Rajeshwaranj <110368828+Rajeshwaranj@users.noreply.github.com>
2022-08-27 01:10:01 +0530

    Update UserRepository.java

--- CustomerManager/src/net/codejava/user/UserRepository.java
+++ CustomerManager/src/net/codejava/user/UserRepository.java
@@ -8,8 +8,6 @@ import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends CrudRepository<User, Long> {
 	
-	@Query(value = "SELECT c FROM User c WHERE c.name LIKE '%' || :keyword || '%'"
-			+ " OR c.email LIKE '%' || :keyword || '%'"
-			+ " OR c.address LIKE '%' || :keyword || '%'")
-	public List<User> search(@Param("keyword") String keyword);
+	@Query(value = "SELECT c FROM User c WHERE c.id LIKE '%' || :keyword || '%'")
+	public List<User> search(@Param("keyword") int keyword);
 }
